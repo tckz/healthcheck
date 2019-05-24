f6e98ee07b8dc71e054f7e5a70d0eaf8a78fde3c
tckz <at.tckz@gmail.com>
2019-05-24 10:42:21 +0900

    simpler

--- response_writer_wrapper.go
+++ response_writer_wrapper.go
@@ -2,31 +2,20 @@ package healthcheck
 
 import "net/http"
 
-// ResponseWriterWrapper StatusCodeを後から参照できるようにする
 type ResponseWriterWrapper struct {
 	http.ResponseWriter
 
 	StatusCode int
-
-	orgWriter http.ResponseWriter
 }
 
 func NewResponseWriterWrapper(w http.ResponseWriter) *ResponseWriterWrapper {
 	return &ResponseWriterWrapper{
-		StatusCode: http.StatusOK,
-		orgWriter:  w,
+		ResponseWriter: w,
+		StatusCode:     http.StatusOK,
 	}
 }
 
-func (w *ResponseWriterWrapper) Header() http.Header {
-	return w.orgWriter.Header()
-}
-
-func (w *ResponseWriterWrapper) Write(b []byte) (int, error) {
-	return w.orgWriter.Write(b)
-}
-
 func (w *ResponseWriterWrapper) WriteHeader(code int) {
-	w.orgWriter.WriteHeader(code)
+	w.ResponseWriter.WriteHeader(code)
 	w.StatusCode = code
 }
